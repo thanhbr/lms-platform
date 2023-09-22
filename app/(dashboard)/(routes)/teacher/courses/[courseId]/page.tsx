8a352aaa1177cddf86258f649db73071ccfbe5e1
ThanhMaThe <thanh.mathe@upos.vn>
2023-09-22 10:14:21 +0700

    update

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
@@ -1,6 +1,11 @@
+import { IconBadge } from "@/components/icon-badge";
 import { db } from "@/lib/db";
 import { auth } from "@clerk/nextjs";
 import { redirect } from "next/navigation";
+import { LayoutDashboard } from "lucide-react";
+
+import { TitleForm } from "./_components/title-form";
+import { DescriptionForm } from "./_components/description-form";
 
 const CourseIdPage = async ({
   params
@@ -25,6 +30,7 @@ const CourseIdPage = async ({
 
   const requiredFields = [ 
     course.title,
+    course.description,
     course.imageUrl,
     course.price,
     course.categoryId
@@ -42,9 +48,29 @@ const CourseIdPage = async ({
           <h1 className="text-2xl font-medium">
             Course setup
           </h1>
-          <span>
+          <span className="text-sm text-slate-700">
             Complete all fields {completionText}
           </span>
+        </div>
+      </div>
+      <div className="grid grid-cols-1 md:grid-col-2 gap-6 mt-16">
+        <div>
+          <div className="flex items-center gap-x-2">
+            <IconBadge  
+              icon={LayoutDashboard}
+            />
+            <h2 className="text-xl">
+              Customize your course
+            </h2>
+          </div>
+          <TitleForm
+            initialData={course}
+            courseId={course.id}
+          />
+          <DescriptionForm
+            initialData={course}
+            courseId={course.id}
+          />
         </div>
       </div>
     </div>
