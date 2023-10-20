6f3757b6ab5a08536146f38c8d58c0304166ef9f
ThanhMaThe <thanh.mathe@upos.vn>
2023-10-20 17:30:41 +0700

    update

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
@@ -2,7 +2,7 @@ import { IconBadge } from "@/components/icon-badge";
 import { db } from "@/lib/db";
 import { auth } from "@clerk/nextjs";
 import { redirect } from "next/navigation";
-import { LayoutDashboard } from "lucide-react";
+import { LayoutDashboard, ListChecks } from "lucide-react";
 
 import { TitleForm } from "./_components/title-form";
 import { DescriptionForm } from "./_components/description-form";
@@ -62,36 +62,44 @@ const CourseIdPage = async ({
           </span>
         </div>
       </div>
-      <div className="grid grid-cols-1 md:grid-col-2 gap-6 mt-16">
-        <div>
-          <div className="flex items-center gap-x-2">
-            <IconBadge  
-              icon={LayoutDashboard}
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-6 mt-16">
+          <div>
+            <div className="flex items-center gap-x-2">
+              <IconBadge icon={LayoutDashboard} />
+              <h2 className="text-xl">
+                Customize your course
+              </h2>
+            </div>
+            <TitleForm
+              initialData={course}
+              courseId={course.id}
             />
-            <h2 className="text-xl">
-              Customize your course
-            </h2>
+            <DescriptionForm
+              initialData={course}
+              courseId={course.id}
+            />
+            <ImageForm
+              initialData={course}
+              courseId={course.id}
+            />
+            <CategoryForm
+              initialData={course}
+              courseId={course.id}
+              options={categories.map((category) => ({
+                label: category.name,
+                value: category.id,
+              }))}
+            />
           </div>
-          <TitleForm
-            initialData={course}
-            courseId={course.id}
-          />
-          <DescriptionForm
-            initialData={course}
-            courseId={course.id}
-          />
-          <ImageForm
-            initialData={course}
-            courseId={course.id}
-          />
-          <CategoryForm
-            initialData={course}
-            courseId={course.id}
-            options={categories.map((category) => ({
-              label: category.name,
-              value: category.id
-            }))}
-          />
+          <div className="space-y-6">
+            <div>
+              <div className="flex items-center gap-x-2">
+                <IconBadge icon={ListChecks} />
+                <h2 className="text-xl">
+                  Course chapters
+                </h2>
+            </div>
+          </div>
         </div>
       </div>
     </div>
