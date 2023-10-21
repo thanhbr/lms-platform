c599e7ce70547e08f25dc317f9ea3c5e21da9517
ThanhMaThe <thanh.mathe@upos.vn>
2023-10-21 08:38:48 +0700

    update price form

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
@@ -2,12 +2,13 @@ import { IconBadge } from "@/components/icon-badge";
 import { db } from "@/lib/db";
 import { auth } from "@clerk/nextjs";
 import { redirect } from "next/navigation";
-import { LayoutDashboard, ListChecks } from "lucide-react";
+import { CircleDollarSign, LayoutDashboard, ListChecks } from "lucide-react";
 
 import { TitleForm } from "./_components/title-form";
 import { DescriptionForm } from "./_components/description-form";
 import { ImageForm } from "./_components/image-form";
 import { CategoryForm } from "./_components/category-form";
+import { PriceForm } from "./_components/price-form";
 
 const CourseIdPage = async ({
   params
@@ -98,11 +99,26 @@ const CourseIdPage = async ({
                 <h2 className="text-xl">
                   Course chapters
                 </h2>
+              </div>
+              <div>
+                TODO: Chapters
+              </div>
+            </div>
+            <div>
+              <div className="flex items-center gap-x-2">
+                <IconBadge icon={CircleDollarSign} />
+                <h2 className="text-xl">
+                  Sell your course
+                </h2>
+              </div>
+              <PriceForm 
+                initialData={course}
+                courseId={course.id}
+              />
             </div>
           </div>
         </div>
       </div>
-    </div>
   )
 }
  
