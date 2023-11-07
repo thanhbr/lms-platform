237c41f2838e54f4a79f11af9ef7ea506ac0ae19
ThanhMaThe <thanh.mathe@upos.vn>
2023-11-07 17:15:28 +0700

    Course Publish Action

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
@@ -11,6 +11,8 @@ import { CategoryForm } from "./_components/category-form";
 import { PriceForm } from "./_components/price-form";
 import { AttachmentForm } from "./_components/attachment-form";
 import { ChaptersForm } from "./_components/chapters-form";
+import { Banner } from "@/components/banner";
+import Actions from "./_components/actions";
 
 const CourseIdPage = async ({
   params
@@ -65,20 +67,33 @@ const CourseIdPage = async ({
   const completedFields = requiredFields.filter(Boolean)?.length
   const completionText = `(${completedFields} / ${totalFields})`
 
+  const isComplete = requiredFields.every(Boolean);
 
   return ( 
-    <div className="p-6">
-      <div className="flex items-center justify-between">
-        <div className="flex flex-col gap-y-2">
-          <h1 className="text-2xl font-medium">
-            Course setup
-          </h1>
-          <span className="text-sm text-slate-700">
-            Complete all fields {completionText}
-          </span>
+    <>
+      {!course.isPublished && (
+        <Banner 
+          label="This course is unpublished. It will not be visible to the students."
+        />
+      )}
+      <div className="p-6">
+        <div className="flex items-center justify-between">
+          <div className="flex flex-col gap-y-2">
+            <h1 className="text-2xl font-medium">
+              Course setup
+            </h1>
+            <span className="text-sm text-slate-700">
+              Complete all fields {completionText}
+            </span>
+          </div>
+          {/* Add Actions */}
+          <Actions 
+            disabled={!isComplete}
+            courseId={params.courseId}
+            isPublished={course.isPublished}
+          />
         </div>
-      </div>
-      <div className="grid grid-cols-1 md:grid-cols-2 gap-6 mt-16">
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-6 mt-16">
           <div>
             <div className="flex items-center gap-x-2">
               <IconBadge icon={LayoutDashboard} />
@@ -147,6 +162,7 @@ const CourseIdPage = async ({
           </div>
         </div>
       </div>
+    </>
   )
 }
  
