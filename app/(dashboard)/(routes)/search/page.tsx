32553171646f9f8cdc01be1e672bdb64d7945e93
ThanhMaThe <thanh.mathe@upos.vn>
2023-11-08 17:28:55 +0700

    wip Course Grid &  Card UI

--- app/(dashboard)/(routes)/search/page.tsx
+++ app/(dashboard)/(routes)/search/page.tsx
@@ -1,12 +1,37 @@
 import { db } from "@/lib/db";
 import { Categories } from "./_components/categories";
 import { SearchInput } from "@/components/search-input";
+import { auth } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+import { getCourses } from "@/actions/get-courses";
+import { CoursesList } from "@/components/courses-list";
 
-const SearchPage = async () => {
+
+interface SearchPageProps {
+  searchParams: {
+    title: string;
+    categoryId: string;
+  }
+}
+
+const SearchPage = async ({
+  searchParams
+}: SearchPageProps) => {
+  const { userId } = auth();
+
+  if(!userId) {
+    return redirect("/");
+  }
+
   const categories = await db.category.findMany({
     orderBy: {
       name: "asc"
     }
+  });
+
+  const courses = await getCourses({
+    userId,
+    ...searchParams
   });
 
   return ( 
@@ -17,6 +42,9 @@ const SearchPage = async () => {
       <div className="p-6">
         <Categories 
           items={categories}
+        />
+        <CoursesList 
+          items={courses}
         />
       </div>
     </>
---
+++ components/course-card.tsx
@@ -0,0 +1,42 @@
+import Image from "next/image";
+import Link from "next/link";
+
+interface CourseCardProps {
+  id: string;
+  title: string;
+  imageUrl: string;
+  chaptersLength: number;
+  price: number;
+  progress: number | null;
+  category: string;
+}
+
+export const CourseCard = ({
+  id,
+  title,
+  imageUrl,
+  chaptersLength,
+  price,
+  progress,
+  category
+}: CourseCardProps) => {
+  return (
+    <Link href={`/courses/${id}`}>
+      <div className="group hover:shadow-sm transition overflow-hidden border rounded-lg p-3 h-full">
+        <div className="relative w-full aspect-video rounded-md overflow-hidden">
+          <Image 
+            fill
+            className="object-cover"
+            alt={title}
+            src={imageUrl}
+          />
+        </div>
+        <div className="flex flex-col pt-2">
+          <div className="text-lg md:text-base font-medium group-hover:text-sky-700 transition line-clamp-2">
+            {title}
+          </div>
+        </div>
+      </div>
+    </Link>
+  )
+}
