364520d1285667ef4a049f3af1734e6a2959326e
ThanhMaThe <thanh.mathe@upos.vn>
2023-11-08 15:36:04 +0700

    Search Query

--- components/navbar-routes.tsx
+++ components/navbar-routes.tsx
@@ -4,36 +4,45 @@ import { usePathname } from "next/navigation"
 import { LogOut } from "lucide-react"
 import { Button } from "./ui/button";
 import Link from "next/link";
+import { SearchInput } from "./search-input";
 
 const NavbarRoutes = () => {
-  const pathname = usePathname()
+  const pathname = usePathname();
 
-  const isTecherPage = pathname?.startsWith("/teacher")
-  const isPlayerPage = pathname?.includes("/chapter")
+  const isTecherPage = pathname?.startsWith("/teacher");
+  const isPlayerPage = pathname?.includes("/chapter");
+  const isSearchPage = pathname === "/search";
 
   return ( 
-    <div className="flex gap-x-2 ml-auto">
-      {isTecherPage || isPlayerPage 
-        ? (
-          <Link href="/">
-            <Button size="sm" variant="ghost">
-              <LogOut className="h-4 w-4 mr-2" />
-              Exit
-            </Button>
-          </Link>
-        )
-        : (
-          <Link href="/teacher/courses">
-            <Button size="sm" variant="ghost">
-              Teacher mode
-            </Button>
-          </Link>
-        )
-      }
-      <UserButton 
-        afterSignOutUrl="/"
-      />
-    </div>
+    <>
+      {isSearchPage && (
+        <div className="hidden md:block">
+          <SearchInput />
+        </div>
+      )}
+      <div className="flex gap-x-2 ml-auto">
+        {isTecherPage || isPlayerPage 
+          ? (
+            <Link href="/">
+              <Button size="sm" variant="ghost">
+                <LogOut className="h-4 w-4 mr-2" />
+                Exit
+              </Button>
+            </Link>
+          )
+          : (
+            <Link href="/teacher/courses">
+              <Button size="sm" variant="ghost">
+                Teacher mode
+              </Button>
+            </Link>
+          )
+        }
+        <UserButton 
+          afterSignOutUrl="/"
+        />
+      </div>
+    </>
   )
 }
 
