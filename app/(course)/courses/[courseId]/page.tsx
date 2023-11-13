4dd6bc6b3456c8542c0a0977914ee046e4880b1e
ThanhMaThe <thanh.mathe@upos.vn>
2023-11-13 15:46:55 +0700

    Chapter HLS Video Player

--- app/(course)/courses/[courseId]/page.tsx
+++ app/(course)/courses/[courseId]/page.tsx
@@ -1,17 +1,34 @@
 import { db } from "@/lib/db";
+import { redirect } from "next/navigation";
 
-const CourseIdPage = ({
+const CourseIdPage = async ({
   params
 }: {
   params: {
     courseId: string;
   }
 }) => {
-  return ( 
-    <div>
-      Watch the course
-    </div>
-   );
+  const course = await db.course.findUnique({
+    where: {
+      id: params.courseId
+    },
+    include: {
+      chapters: {
+        where: {
+          isPublished: true
+        },
+        orderBy: {
+          position: "asc"
+        }
+      }
+    }
+  });
+
+  if(!course) {
+    return redirect("/");
+  }
+
+  return redirect(`/courses/${course.id}/chapters/${course.chapters[0].id}`);
 }
  
 export default CourseIdPage;
