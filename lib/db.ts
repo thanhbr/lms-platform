ee41c2aba40c9a397180f371657b1135563be818
ThanhMaThe <thanh.mathe@upos.vn>
2024-01-16 11:32:19 +0700

    update config db

--- lib/db.ts
+++ lib/db.ts
@@ -6,4 +6,5 @@ declare global {
 
 export const db = globalThis.prisma || new PrismaClient()
 
-if(process.env.NODE_ENV !== "production") globalThis.prisma = db
+// if(process.env.NODE_ENV !== "production") globalThis.prisma = db
+globalThis.prisma = db
